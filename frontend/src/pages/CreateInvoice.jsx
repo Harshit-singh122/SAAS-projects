ba0a8ccce43afb34695895b81885d4498548d418
Harshit singh <singhunny122@gmail.com>
2026-02-15 15:04:03 +0530

    Fix import statement syntax in CreateInvoice.jsx

--- frontend/src/pages/CreateInvoice.jsx
+++ frontend/src/pages/CreateInvoice.jsx
@@ -5,7 +5,7 @@ import StatusBadge from "../components/StatusBadge.jsx";import {
   createInvoiceStyles,
   createInvoiceIconColors,
   createInvoiceCustomStyles,
-} from from "../assets/dummystyle.js";
+} from "../assets/dummystyle.js";
 /* ---------- API BASE ---------- */
 const API_BASE = "http://localhost:4000";
 
