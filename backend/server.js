95189647d828a54389c426a1fdc5c0a8b8caf7a8
Harshit singh <singhunny122@gmail.com>
2026-02-15 20:21:32 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -14,9 +14,14 @@ const PORT = 4000;
 
 // Middleware
 app.use(cors({
-  origin: ["http://localhost:5173", "http://localhost:5174"],
+  origin: [
+    "http://localhost:5173",
+    "http://localhost:5174",
+    "https://YOUR_FRONTEND_NAME.vercel.app"
+  ],
   credentials: true
 }));
+
 app.use(clerkMiddleware());
 app.use(express.json({ limit: '20mb' }));
 app.use(express.urlencoded({ extended: true, limit: '20mb' }));
@@ -37,5 +42,6 @@ app.get('/', (req, res) => {
 });
 
 app.listen(PORT, () => {
-  console.log(`Server running at http://localhost:${PORT}`);
+  console.log(`Server running on port ${PORT}`);
+
 });
